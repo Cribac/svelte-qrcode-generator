abf4bab8b36309e8333c547f539febde72b1d636
Christian Bachhelm <cbachhelm@tuta.io>
2021-11-07 17:06:16 +0100

    Scaffold a test image.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,24 @@
+<script>
+	import QRCode from 'qrcode';
+
+	let data = {
+		name: "Fooberto Testino",
+		department: "Marketing",
+		website: "testino.tld",
+		id: "aisuoiqu3234738jdhf100223",
+	};
+
+	let stringdata = JSON.stringify(data);
+
+	let qrImage;
+
+	QRCode.toDataURL(stringdata, function (err, url) {
+		if(err) return console.log("error occured");
+		qrImage = url;
+	});
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<img alt="QR Code" src="{qrImage}" />
