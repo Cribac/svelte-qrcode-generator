abffc027b2d2ccaa7c97631959f4130a2e1dc9a3
Christian Bachhelm <cbachhelm@tuta.io>
2021-12-28 20:17:48 +0100

    Add custom colors.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,17 @@
 module.exports = {
   content: ['./src/**/*.svelte', './src/**/*.css'],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#FFFFFF',
+      snow: '#F6FAFB',
+      highlight: '#D49C6B',
+      greyish: '#C1C8C7',
+      tundra: '#5E6668',
+      midnight: '#2B2E33',
+      night: '#252B31',
+    },
     extend: {},
   },
   plugins: [],
