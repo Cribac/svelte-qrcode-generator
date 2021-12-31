966c96ace8e5ce1f9d013a2d260c2edffc32e178
Christian Bachhelm <cbachhelm@tuta.io>
2021-12-31 13:48:39 +0100

    Ass home route.

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,7 @@
 /**
  * Define the routes of the app that are used in navigation bars, menus etc.
  */
-export const routes:[Route] = [
+export const routes:Route[] = [
+	{ path: '/', text: 'Home' },
 	{ path: '/generators/text', text: 'Textinput' },
 ];
