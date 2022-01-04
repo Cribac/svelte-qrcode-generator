3b4883d006162d1bbf0b9813f204364dad6544ad
Christian Bachhelm <cbachhelm@tuta.io>
2022-01-04 12:24:57 +0100

    Collapse OffCanvasMenu on route changes.

--- src/components/navigation/OffCanvasMenu.svelte
+++ src/components/navigation/OffCanvasMenu.svelte
@@ -1,10 +1,34 @@
 <script lang="ts">
 	import OffCanvasMenuItem from './OffCanvasMenuItem.svelte';
 	import { routes } from '../../routes/routes';
+	import { page } from "$app/stores";
+	/**
+	 * Flag to determine if the menu is toggled or not.
+	 *
+	 * @type {!boolean}
+	 */
+	let toggled = false;
+
+	/**
+	 * Reactive property to check if menu items are active.
+	 *
+	 * @type {!boolean}
+	 */
+	$: isActive = (route) => (route.path===$page.path);
+
+	/**
+	 * Function to set the toggled state.
+	 *
+	 * @param route {Route}
+	 */
+	// eslint-disable-next-line no-undef
+	function handleToggled(route:Route):void {
+		toggled = (route.path===$page.path);
+	}
 </script>
 
 <aside class="bg-tundra">
-	<input type="checkbox" id="menu-toggle">
+	<input type="checkbox" id="menu-toggle" bind:checked={toggled}>
 	<label class="btn flex items-center text-white" for="menu-toggle" data-testid="sidebar">
 		<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 			<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -14,7 +38,18 @@
 	<div class="sidebar bg-midnight fixed">
 		<ul>
 			{#each routes as route}
-				<OffCanvasMenuItem {route} />
+				{#if isActive(route)}
+					<li class="px-2 py-3 bg-snow">
+						<OffCanvasMenuItem {route} isActive=true />
+					</li>
+				{:else}
+					<li
+						class="px-2 py-3 hover:bg-snow hover:border-t hover:border-solid hover:border-midnight group"
+						on:click={() => handleToggled(route)}
+					>
+						<OffCanvasMenuItem {route} />
+					</li>
+				{/if}
 			{/each}
 		</ul>
 	</div>
--- src/components/navigation/OffCanvasMenuItem.svelte
+++ src/components/navigation/OffCanvasMenuItem.svelte
@@ -1,17 +1,10 @@
 <script lang="ts">
-	import { page } from "$app/stores";
-
 	export let route;
-
-	$: isActive = (route.path===$page.path);
+	export let isActive = false;
 </script>
 
 {#if isActive}
-	<li class="px-2 py-3 bg-snow">
-		<a class="p-1 text-base cursor-default text-midnight" href="{route.path}">{route.text}</a>
-	</li>
+	<a class="p-1 text-base cursor-default text-midnight" href="{route.path}">{route.text}</a>
 {:else}
-	<li class="px-2 py-3 hover:bg-snow hover:border-t hover:border-solid hover:border-midnight group">
-		<a class="p-1 text-base text-white group-hover:text-midnight" href="{route.path}">{route.text}</a>
-	</li>
+	<a class="p-1 text-base text-white group-hover:text-midnight" href="{route.path}">{route.text}</a>
 {/if}
