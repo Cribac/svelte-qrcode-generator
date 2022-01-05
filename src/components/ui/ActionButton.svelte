dd904d369b64e4e31c1a70000d24069669e89931
Christian Bachhelm <cbachhelm@tuta.io>
2022-01-05 17:31:45 +0100

    Add option to display an icon to ActionButtons.

--- src/components/ui/ActionButton.svelte
+++ src/components/ui/ActionButton.svelte
@@ -16,7 +16,9 @@
 
 <button
   type="button"
+  class="bg-tundra hover:bg-greyish text-snow font-bold py-2 px-4 rounded inline-flex items-center"
   on:click={action}
 >
+  <slot></slot>
   {buttonText}
 </button>
--- src/routes/generators/text.svelte
+++ src/routes/generators/text.svelte
@@ -45,7 +45,11 @@
 	></textarea>
 
 	{#if text.length > 0}
-		<ActionButton action={handleGenerate} buttonText="Generate QRCode" />
+		<ActionButton action={handleGenerate} buttonText="Generate QRCode">
+			<svg xmlns="http://www.w3.org/2000/svg" class="fill-current h-4 w-4 mr-2" viewBox="0 0 20 20" fill="currentColor">
+				<path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
+			</svg>
+		</ActionButton>
 	{/if}
 
 	{#if generateOk && text.length === 0}
@@ -60,9 +64,21 @@
 		</div>
 
 		<div>
-			<ActionButton action={handleDownload} buttonText="Download" />
-			<ActionButton action={handleShare} buttonText="Share" />
-			<ActionButton action={handleRestart} buttonText="Restart" />
+			<ActionButton action={handleDownload} buttonText="Download">
+				<svg xmlns="http://www.w3.org/2000/svg" class="fill-current h-4 w-4 mr-2" viewBox="0 0 20 20" fill="currentColor">
+					<path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
+				</svg>
+			</ActionButton>
+			<ActionButton action={handleShare} buttonText="Share">
+				<svg xmlns="http://www.w3.org/2000/svg" class="fill-current h-4 w-4 mr-2" viewBox="0 0 20 20" fill="currentColor">
+					<path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" />
+				</svg>
+			</ActionButton>
+			<ActionButton action={handleRestart} buttonText="Restart">
+				<svg xmlns="http://www.w3.org/2000/svg" class="fill-current h-4 w-4 mr-2" viewBox="0 0 20 20" fill="currentColor">
+					<path fill-rule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clip-rule="evenodd" />
+				</svg>
+			</ActionButton>
 		</div>
 	{/if}
 
