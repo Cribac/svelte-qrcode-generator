bacc991095d807b1d8dfb058d76cc87bc2afd3d8
Christian Bachhelm <cbachhelm@tuta.io>
2022-01-05 13:32:04 +0100

    Scaffold textinput qr-code generation.

--- src/routes/generators/text.svelte
+++ src/routes/generators/text.svelte
@@ -1,1 +1,54 @@
+<script>
+	import QRCode from 'qrcode';
+
+	let text = '';
+
+	let stringdata = '';
+
+	let generateOk = false;
+
+	function handleGenerate() {
+		stringdata = JSON.stringify(text);
+		generateOk = true;
+		text = '';
+	}
+
+	async function generateQr(text) {
+		return QRCode.toDataURL(text);
+	}
+</script>
+
 <p>TEXT</p>
+<div class="flex flex-col">
+	<label for="input-text">
+		Please input your text
+	</label>
+	<textarea
+		id="input-text"
+		type="text"
+		class="flex-grow h-64 p-2"
+		bind:value={text}
+	></textarea>
+
+	<p>text: {text}</p>
+	<p>stringdata: {stringdata}</p>
+
+	{#if text.length > 0}
+		<button
+			type="button"
+			on:click={handleGenerate}
+		>
+			Generate QRCode
+		</button>
+	{/if}
+
+	{#if generateOk && text.length === 0}
+		{#await generateQr(stringdata)}
+			<div>generating...</div>
+		{:then dataString}
+			<img alt="QR Code" src="{dataString}" />
+		{:catch error}
+			<p style="color: red">{error.message}</p>
+		{/await}
+	{/if}
+</div>
