fc26e6155d61fd67c8d4104bd0dd5bc96263386c
Christian Bachhelm <cbachhelm@tuta.io>
2022-01-06 20:14:54 +0100

    Add button to restart generation workflow on errors.

--- src/routes/generators/text.svelte
+++ src/routes/generators/text.svelte
@@ -24,14 +24,14 @@
 	 *
 	 * @type {string}
 	 */
-	let error;
+	let error = '';
 
 	/**
 	 * Flag to track a loading state.
 	 *
 	 * @type {boolean}
 	 */
-	let loading;
+	let loading = false;
 
 	/**
 	 * Reactive flag to check if the QRCode  generation was successful.
@@ -46,6 +46,7 @@
 	function resetInputs() {
 		text = '';
 		encodedData = '';
+		error = '';
 	}
 
 	async function generateQrCode(text) {
@@ -58,6 +59,7 @@
 				loading = false;
 			}
 		} catch (err) {
+			loading = false;
 			error = err.message;
 		}
 	}
@@ -93,7 +95,13 @@
 
 	{#if error}
 		<div class="mt-4">
-			<Alert alertType="error" message={error} />
+			<Alert alertType="error" message={error}>
+				<ActionButton slot="button" action={() => resetInputs()} buttonText="Restart">
+					<svg xmlns="http://www.w3.org/2000/svg" class="fill-current h-4 w-4 mr-2" viewBox="0 0 20 20" fill="currentColor">
+						<path fill-rule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clip-rule="evenodd" />
+					</svg>
+				</ActionButton>
+			</Alert>
 		</div>
 	{/if}
 
