48c1383b6119a7779f0670dd9ea82efe506234a2
Christian Bachhelm <cbachhelm@tuta.io>
2022-01-08 21:54:07 +0100

    Update test cases for plain text qrcode generation.

--- src/__tests__/routes/generators/text.test.ts
+++ src/__tests__/routes/generators/text.test.ts
@@ -5,10 +5,21 @@
 import '@testing-library/jest-dom'
 import Text from '../../../routes/generators/text.svelte';
 import { render } from '@testing-library/svelte';
+import userEvent from '@testing-library/user-event'
 
-describe('Textinput route works', () => {
+describe('QRCode generation for plain text works', () => {
   test('Render default', () => {
-    const { getByLabelText } = render(Text);
-    expect(getByLabelText(/Please input your text/)).toBeInTheDocument();
+    const { getByLabelText, getByRole } = render(Text);
+    expect(getByLabelText('Please enter your text')).toBeInTheDocument();
+    const area = getByRole('textbox');
+    expect(area).toBeVisible();
+  });
+
+  test('Text input displays a generate button', async () => {
+    const { getByRole, findByRole } = render(Text);
+    const area = getByRole('textbox');
+    userEvent.type(area, 'Run you fools!');
+    const button = await findByRole('button');
+    expect(button).toHaveTextContent('Generate QRCode');
   });
 });
