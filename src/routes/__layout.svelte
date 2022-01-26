203c0494789f230104ebd9bc6d6b341883406468
Christian Bachhelm <cbachhelm@tuta.io>
2022-01-26 21:15:30 +0100

    Provide dynamic page titles.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
 	import '../styles/tailwind.css';
 	import OffCanvasMenu from '$lib/components/navigation/OffCanvasMenu.svelte';
+  import { page } from '$app/stores';
+  import { routes } from './routes';
+
+  /**
+   * @type {Route}
+   */
+  $: title = routes.find(t => t.path === $page.url.pathname);
 </script>
+
+<svelte:head>
+  <title>QRCode Generator | {title.text}</title>
+</svelte:head>
 
 <header>
   <OffCanvasMenu />
