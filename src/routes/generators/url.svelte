dc2a1e4067e5504a0d796520e16e4f5b47b34812
Christian Bachhelm <cbachhelm@tuta.io>
2022-03-02 21:06:01 +0100

    Rearrange button order for url generation.

--- src/routes/generators/url.svelte
+++ src/routes/generators/url.svelte
@@ -106,16 +106,6 @@
   />
 </div>
 
-{#if isValidUrl}
-  <div class="mt-4">
-    <ActionButton action={() => generateQrCode(url)} buttonText="Generate QRCode">
-      <svg xmlns="http://www.w3.org/2000/svg" class="fill-current h-4 w-4 mr-2" viewBox="0 0 20 20" fill="currentColor">
-        <path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
-      </svg>
-    </ActionButton>
-  </div>
-{/if}
-
 {#if loading}
   <div class="mt-4">
     <LoadingIndicator text="generating..." />
@@ -150,6 +140,16 @@
 {/if}
 
 <div class="flex justify-between sm:w-1/2">
+  {#if isValidUrl}
+    <div class="mt-4 text-sm w-1/2 mr-1">
+      <ActionButton action={() => generateQrCode(url)} buttonText="Generate QRCode">
+        <svg xmlns="http://www.w3.org/2000/svg" class="fill-current h-4 w-4 mr-2" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
+        </svg>
+      </ActionButton>
+    </div>
+  {/if}
+
   {#if encodedUrl}
     <div class="mt-4 text-sm w-1/2 mr-1">
       <DownloadButton data={encodedUrl} buttonText="Download" fileName="qrcode-url.png">
